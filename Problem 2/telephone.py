5ba1bbb9bc4c0cf3f8fb595a9a3ea5854086fa51
Mason Anderson <mta0627@utulsa.edu>
2024-02-02 15:25:05 -0600

    wrote code for problem 2

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,12 @@
 def telephone():
     phone_number = int(input())
     ''' Type your code here. '''
+    
+    area_code = phone_number // 10**7
+    prefix = phone_number // 10**4 % 10**3
+    line_number = phone_number % 10**4
+
+    print(f'({area_code}) {prefix}-{line_number}')
+    
 if __name__ == "__main__":
     telephone()
