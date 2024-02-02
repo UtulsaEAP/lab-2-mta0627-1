4363f904108bcfe687a17dbf6d2814b839e046ba
Mason Anderson <mta0627@utulsa.edu>
2024-02-02 15:33:14 -0600

    Wrote code for problem 3

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -3,5 +3,13 @@ def caffeine():
     caffeine_mg = float(input())
     ''' Type your code here. '''
     
+    caffeine_level_after_6_hrs = caffeine_mg / 2
+    caffeine_level_after_12_hrs = caffeine_mg / 4
+    caffeine_level_after_24_hrs = caffeine_mg / 16
+
+    print(f'After 6 hours: {caffeine_level_after_6_hrs:.2f} mg')
+    print(f'After 12 hours: {caffeine_level_after_12_hrs:.2f} mg')
+    print(f'After 24 hours: {caffeine_level_after_24_hrs:.2f} mg')
+
 if __name__ == "__main__":
     caffeine()
